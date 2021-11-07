587602b4ea51655616c12e046b10a98c29c9454c
Kruti Chodwadia <chodwadiakruti@gmail.com>
2021-11-07 12:48:37 +0100

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SSE_TASK_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SSE_TASK_1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c2b7cf35-3235-41f5-b873-c941acc61bee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SSE_TASK_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+      
+        public int Multiply(int a, int b) {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Divide by zero is not allowed");
+                return 0;
+            }
+            return a / b;
+        }
+
+        [TestMethod]
+        public void testMutiply()
+        {
+            Assert.AreEqual(6, Multiply(3, 2));
+        }
+
+        [TestMethod]
+        public void testDivide()
+        {
+            Assert.AreEqual(2, Divide(4, 2));
+        }
+
+        [TestMethod]
+        public void testDivideByZero()
+        {
+            Assert.AreEqual(0, Divide(4, 0));
+        }
+
+    }
+}
